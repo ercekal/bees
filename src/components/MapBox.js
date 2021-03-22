c4fc5d7065d23075870f07cbc4a9ce61fa3b720e
erce kalabalikoglu <erce@erces-MacBook-Pro-2.local>
2021-03-22 16:42:19 +0000

    map mobile is also done

--- src/components/MapBox.js
+++ src/components/MapBox.js
@@ -6,21 +6,40 @@ import BarlowText from './BarlowText'
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  width: 13.25rem;
-  height: 7.5rem;
-  margin: 1rem 1rem 0 0;
   background-color: #ffff00;
   justify-content: flex-start;
   align-items: center;
-  padding-top: 1rem;
+  margin-bottom: 1rem;
+  padding: 1rem;
+  width: 100%;
+  @media (min-width: 768px) {
+    margin: 1rem 1rem 0 0;
+    width: 13.25rem;
+    padding-top: 1rem;
+    height: 7.5rem;
+  }
+`
+
+const Mobile = styled.div`
+  @media (min-width: 768px) {
+    display: none;
+  }
+`
+
+const Desktop = styled.div`
+  display: none;
+  @media (min-width: 768px) {
+    display: initial;
+  }
 `
 
 const TextContainer = styled.div`
-  width: 80%;
-  padding-bottom: 0.5rem;
+  @media (min-width: 768px) {
+    padding-bottom: 0.5rem;
+  }
 `
 
-const renderText = (item, i) => {
+const renderTextDesktop = (item, i) => {
   const { text, bold } = item
 
   if (item.bold) {
@@ -41,10 +60,36 @@ const renderText = (item, i) => {
   )
 }
 
+const renderTextMobile = (item, i) => {
+  const { text, bold } = item
+
+  if (item.bold) {
+    return (
+      <TextContainer>
+        <BarlowText key={i} size="28px" lineHeight="33.6px">
+          {item.text}
+        </BarlowText>
+      </TextContainer>
+    )
+  }
+  return (
+    <TextContainer>
+      <WorkSans key={i} lineHeight="16px" size="14px">
+        {item.text}
+      </WorkSans>
+    </TextContainer>
+  )
+}
+
 const MapBox = ({ items }) => {
   return (
     <Container>
-      {items.map((item, i) => renderText(item, i))}
+      <Desktop>
+        {items.map((item, i) => renderTextDesktop(item, i))}
+      </Desktop>
+      <Mobile>
+        {items.map((item, i) => renderTextMobile(item, i))}
+      </Mobile>
     </Container>
   )
 }
