957dd79e7baf32121228fc24b1a9da86955ee268
erce kalabalikoglu <erce@erces-MacBook-Pro-2.local>
2021-03-24 15:06:34 +0000

    map continue

--- src/components/Map2.js
+++ src/components/Map2.js
@@ -3,15 +3,68 @@ import styled from 'styled-components'
 import './Map.css'
 import WorldMap from './WorldMap'
 
-const H2 = styled.h2`
+const Header = styled.h2`
   font-size: 48px;
   line-height: 56px;
   letter-spacing: -3px;
   font-family: 'Barlow Semi Condensed', sans-serif;
   font-weight: 600;
+  margin-bottom: 20px;
 `
 
-const Map = () => {
+const H2 = styled.h2`
+  font-size: 36px;
+  line-height: 43.2px;
+  letter-spacing: -3px;
+  font-family: 'Barlow Semi Condensed', sans-serif;
+  font-weight: 600;
+  margin-bottom: 0;
+`
+
+const P = styled.p`
+  font-size: 14px;
+  line-height: 16.2px;
+  font-family: 'Work Sans', sans-serif;
+  font-weight: 400;
+  margin-bottom: 0;
+`
+
+const Card = styled.div`
+  /* display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  background-color: #ffff00;
+   */
+
+  /* margin: 0 10px 10px 0; */
+  /* padding: 1rem; */
+  /* width: 230px; */
+  /* height: 150px; */
+`
+
+const Map = ({ mapBoxes }) => {
+  const renderText = (item, i) => {
+    const { text, bold } = item
+    return bold ? <H2 key={i}>{text}</H2> : <P key={i}>{text}</P>
+  }
+
+  const list = mapBoxes.mapBoxes.map((item, i) => {
+    return (
+      <div className="box">
+        {item.boxElement.map((el, i) => renderText(el, i))}
+        {/* <h2>Over 1 Million</h2>
+          <p>Retailers</p> */}
+      </div>
+      // <div style={{ float: 'left' }} key={i}>
+      //   <Card className="img-card">
+      //     <div className="img-card-text">
+      //       {item.boxElement.map((el, i) => renderText(el, i))}
+      //     </div>
+      //   </Card>
+      // </div>
+    )
+  })
+
   return (
     <section id="four-boxes" style={{ height: '900px' }}>
       <div className="interactive-maps">
@@ -20,7 +73,7 @@ const Map = () => {
 
       <div className="container clearfix">
         <div className="cols8-map" style={{ marginRight: '4.16%' }}>
-          <H2>And we’re just getting started</H2>
+          <Header>And we’re just getting started</Header>
           <p>
             Every day brings new challenges. Which is why, every day,
             we’re developing new ways to make BEES, better, for
@@ -28,7 +81,7 @@ const Map = () => {
           </p>
 
           <div className="boxes">
-            <div className="box">
+            {/* <div className="box">
               <h2>Over 1 Million</h2>
               <p>Retailers</p>
             </div>
@@ -43,7 +96,8 @@ const Map = () => {
             <div className="box">
               <h2>Over 1 Million</h2>
               <p>Retailers</p>
-            </div>
+            </div> */}
+            {list.map(l => l)}
           </div>
         </div>
       </div>
